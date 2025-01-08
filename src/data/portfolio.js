79c9db18e85bb337d4d022cdbab35ccf695257d3
Kenneth Hendricks <50819541+kenhendricks00@users.noreply.github.com>
2025-01-07 20:32:24 -0500

    Main

---
+++ src/data/portfolio.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        title: '💻 Developer Portfolio',
+        imgUrl: '/assets/dev-portfolio.jpg',
+        stack: ['React', 'Tailwind', 'Vite'],
+        link: 'https://kenhendricks.me',
+    },
+    {
+        title: '🥳 Partycord - Discord Bot',
+        imgUrl: '/assets/partycord.jpg',
+        stack: ['Node.js', 'Discord.js', 'HTML', 'Bootstrap'],
+        link: 'https://partycord.xyz',
+    },
+        {
+        title: '🔗 FixEmbed - Discord Bot',
+        imgUrl: '/assets/fixembed.jpg',
+        stack: ['Python', 'Discord.py'],
+        link: 'https://github.com/kenhendricks00/FixEmbedBot',
+    },
+    {
+        title: '🧰 More Projects',
+        imgUrl: '/assets/projects.jpg',
+        stack: ['Fun', 'Useful', 'Interesting'],
+        link: 'https://kenhendricks.me/projects',
+    }
+]
---
+++ src/data/timeline.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        year: 'Today',
+        title: "Associate's Degree in Computer Science",
+        duration: 'currently',
+        details: 
+            "I'm currently attending the Community College of Philadelphia and pursuing the knowledge that a computer science degree has to offer. I'm learning about computer architecture, computer science theory, an array of programming, and aspects of artificial intelligence.",
+    },
+    {
+        year: '2018',
+        title: 'Highschool',
+        duration: '4 years',
+        details:
+            "I attended New Foundations Charter Highschool, a college prep school, there I took an Engineering class where it was reinforced that I loved the idea of creating something that is useful to people, ultimately earning a 94 in this class.",
+    },
+    {
+        year: '2011',
+        title: 'First Line of Code',
+        duration: 'the beginning',
+        details:
+            "I wrote my first line of real code that started this journey into digital craftsmanship that I never could've imagined, especially as someone who never saw themselves as anything other than a blue collar worker.",
+    },
+]
